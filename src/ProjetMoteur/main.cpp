9af025efebf738e3f86e68d34f6c4cf255d968f6
Baptiste COMBEAU <bcombeau@gaming.tech>
2026-02-05 10:23:33 +0100

    [ADD] Console

--- src/ProjetMoteur/main.cpp
+++ src/ProjetMoteur/main.cpp
@@ -1,7 +1,18 @@
+#include <windows.h>
 #include <iostream>
 #include "main.h"
 
-int WinMain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+    AllocConsole();
+
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+    freopen_s(&f, "CONOUT$", "w", stderr);
+    freopen_s(&f, "CONIN$", "r", stdin);
+
+    std::cout << "Console affichée !" << std::endl;
+
+    system("pause");
     return 0;
 }
