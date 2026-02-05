35ac764e78cca2663f50375f16431c3cbafb73ec
Baptiste COMBEAU <bcombeau@gaming.tech>
2026-02-05 10:02:40 +0100

    [ADD] Engine

---
+++ src/Engine/Window.h
@@ -0,0 +1,17 @@
+#ifndef ENGINE_WINDOW_H
+#define ENGINE_WINDOW_H
+
+class Window
+{
+public:
+	Window();
+	~Window();
+
+	void Create(const char* title, int width, int height);
+	void Destroy();
+	void Update();
+
+private:
+};
+
+#endif // !ENGINE_WINDOW_H
