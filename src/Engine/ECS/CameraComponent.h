c961127d910d2684e94566c15d8ae7d050c09b56
Auxence VILLERET <avilleret@gaming.tech>
2026-02-06 11:31:20 +0100

    [ADD] Component fichier

---
+++ src/Engine/ECS/CameraComponent.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Component.h"
+class CameraComponent : public Component
+{
+};
+
---
+++ src/Engine/ECS/ParticleComponent.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Component.h"
+class ParticleComponent : public Component
+{
+};
+
