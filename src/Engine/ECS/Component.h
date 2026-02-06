63aaa125f2ca1c8fe09b9e0e0f8d5f407c50a06e
Auxence VILLERET <avilleret@gaming.tech>
2026-02-06 11:25:57 +0100

    [REF] Entity + component

--- src/Engine/ECS/Component.h
+++ src/Engine/ECS/Component.h
@@ -3,16 +3,19 @@ class Entity;
 
 class Component
 {
-public:
-	Component() = default;
-	virtual ~Component() = default;
+    virtual ~Component() = default;
 
-	int GetTypeId() { return m_TypeId; }
-	void SetEntity(Entity* entity) { m_Entity = entity; }
-	Entity* GetEntity() { return m_Entity; }
+    // Appelés par les systems
+    virtual void Start() {}
+    virtual void Update(float dt) {}
 
-protected:
-	int m_TypeId = 0;
-	Entity* m_Entity = nullptr;
+    Entity* GetEntity() const { return m_Entity; }
+
+    void SetEntity(Entity* e) { m_Entity = e; }
+
+private:
+    friend class Entity;
+
+    Entity* m_Entity = nullptr;
 };
 
--- src/Engine/ECS/Entity.h
+++ src/Engine/ECS/Entity.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <vector>
+#include <DirectXMath.h>
+
+using namespace DirectX;
 
 class Component;
 
@@ -18,7 +21,53 @@ public:
 	template<typename T>
 	T* GetComponent();
 
+	void IdentityMatrix(XMFLOAT4X4& matrix); //needed ?
+
+	//POSITION
+	void SetPosition(float x, float y, float z); // set XYZ
+	void MoveForward(float dist); // move along the "forward" of the obj
+	void MoveUp(float dist); // move along the "up" of the obj
+	void MoveRight(float dist); // move along the "right" of the obj
+
+	void CoutPosition(); // DEBUG
+
+	//SCALING
+	void SetScale(float scale); // set the scale 
+	void Scale(float scale); // multiply current scale
+	void SetScaleVector(XMFLOAT3 vectorScale); // set the scale with XYZ Vector
+	void ScaleVector(XMFLOAT3 vectorScale); // multiply current scale with XYZ Vector
+
+
+	void CoutScale(); // DEBUG
+
+	//ROTATION
+	void SetRotationX(float angle); // set the rotation
+	void SetRotationY(float angle); // set the rotation
+	void SetRotationZ(float angle); // set the rotation
+
+	void LookAt(float x, float y, float z); // look at a specific coords
+
+	void SetMatrix(float matrixPos, float value); // manually modifiy a value of the matrix
+
+	void CoutRotation(); // DEBUG
+	void CoutMatrix(); // DEBUG
+
+
 private:
+
+	//position
+	XMFLOAT3 m_position;
+
+	//scaling
+	XMFLOAT3 m_scale;
+
+	//rotation
+	XMFLOAT3 m_forward;
+	XMFLOAT3 m_up;
+	XMFLOAT3 m_right;
+
+	XMFLOAT4 m_quaternion;
+	XMFLOAT4X4 m_matrix;
 	uint32_t m_Id = 0;
 	std::vector<Component*> m_Components;
 };
