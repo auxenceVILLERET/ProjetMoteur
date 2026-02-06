e2c379fddd54f74b8d67b994a2967d2d07672380
Auxence VILLERET <avilleret@gaming.tech>
2026-02-06 09:16:31 +0100

    [CLEAR]

--- src/ProjetMoteur/App.cpp
+++ src/ProjetMoteur/App.cpp
@@ -21,35 +21,7 @@ void App::Initialize()
 
 void App::Update()
 {
-	ECS ecs;
-
-	Entity* entity = ecs.CreateEntity<Entity>();
-	PositionComponent* position = entity->AddComponent<PositionComponent>();
-	VelocityComponent* velocity = entity->AddComponent<VelocityComponent>();
-	position->x = 5.0f;
-	position->y = 10.0f;
-	position->z = 0.0f;
-	velocity->vx = 1.0f;
-	velocity->vy = 0.0f;
-	velocity->vz = 5.0f;
-
-	Entity* entity2 = ecs.CreateEntity<Entity>();
-	PositionComponent* position2 = entity2->AddComponent<PositionComponent>();
-	VelocityComponent* velocity2 = entity2->AddComponent<VelocityComponent>();
-	position2->x = -5.0f;
-	position2->y = 0.0f;
-	position2->z = 3.0f;
-	velocity2->vx = 0.0f;
-	velocity2->vy = 2.0f;
-	velocity2->vz = -1.0f;
-
-	ecs.AddSystem<MovementSystem>();
-
-	std::cout << "Entity : " << entity->GetId() << "\n";
-	std::cout << "Position after update: (" << position->x << ", " << position->y << ", " << position->z << ")\n";
-	std::cout << "Entity2 : " << entity2->GetId() << "\n";
-	std::cout << "Position after update: (" << position2->x << ", " << position2->y << ", " << position2->z << ")\n";
-
+	// Update your application here
 }
 
 void App::Shutdown()
